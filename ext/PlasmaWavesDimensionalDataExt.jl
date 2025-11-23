88e6781e68ff78f9fc57eba297b3e0e6e5665389
Beforerr <zzj956959688@gmail.com>
2025-11-22 22:45:18 -0800

    feat: add flexible dimension handling for time series polarization analysis

--- ext/PlasmaWavesDimensionalDataExt.jl
+++ ext/PlasmaWavesDimensionalDataExt.jl
@@ -2,20 +2,24 @@ module PlasmaWavesDimensionalDataExt
 
 using DimensionalData
 using PlasmaWaves
-using PlasmaWaves: wavpol, wavpol_svd, samplingrate, _twavpol
+using PlasmaWaves: wavpol, wavpol_svd, _twavpol
 import PlasmaWaves: twavpol, twavpol_svd
-using DimensionalData.Dimensions: @dim, Dimension
+using DimensionalData.Dimensions: @dim, Dimension, TimeDim, hasdim, dimnum
 
 abstract type FrequencyDim{T} <: Dimension{T} end
 @dim 𝑓 FrequencyDim "Frequency"
+
+# A no-error version of `dimnum`
+_dimnum(x, dim) = hasdim(x, dim) ? dimnum(x, dim) : nothing
 
 _ellipticity(res, dims) = DimArray(res.ellipticity, dims; name = "Ellipticity")
 _power(res, dims) = DimArray(res.power, dims; name = "Power")
 _waveangle(res, dims) = DimArray(res.waveangle, dims; name = "Wave normal angle")
 
-function PlasmaWaves.twavpol(x::AbstractDimArray; kwargs...)
-    res = _twavpol(wavpol, x; kwargs...)
-    dims = (Ti(res.times), 𝑓(res.freqs))
+function PlasmaWaves.twavpol(x::AbstractDimArray; dim = nothing, kwargs...)
+    dim = @something dim _dimnum(x, TimeDim) _dimnum(x, Dim{:time}) 1
+    res = _twavpol(wavpol, x; dim = dim, kwargs...)
+    dims = dim == 1 ? (Ti(res.times), 𝑓(res.freqs)) : (𝑓(res.freqs), Ti(res.times))
     return DimStack(
         (
             power = _power(res, dims),
@@ -27,9 +31,10 @@ function PlasmaWaves.twavpol(x::AbstractDimArray; kwargs...)
     )
 end
 
-function PlasmaWaves.twavpol_svd(x::AbstractDimArray; kwargs...)
-    res = _twavpol(wavpol_svd, x; kwargs...)
-    dims = (Ti(res.times), 𝑓(res.freqs))
+function PlasmaWaves.twavpol_svd(x::AbstractDimArray; dim = nothing, kwargs...)
+    dim = @something dim _dimnum(x, TimeDim) _dimnum(x, Dim{:time}) 1
+    res = _twavpol(wavpol_svd, x; dim = dim, kwargs...)
+    dims = dim == 1 ? (Ti(res.times), 𝑓(res.freqs)) : (𝑓(res.freqs), Ti(res.times))
     return DimStack(
         (
             power = _power(res, dims),
